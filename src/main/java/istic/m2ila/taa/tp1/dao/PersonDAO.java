de4fee1c1bac6320fd19ffb56ab87fb0710a170a
czbirka <christophe.zbirka@wanadoo.fr>
2017-09-22 16:31:50 +0200

    mise en place GET POST DELETE PUT pour Person

--- src/main/java/istic/m2ila/taa/tp1/dao/PersonDAO.java
+++ src/main/java/istic/m2ila/taa/tp1/dao/PersonDAO.java
@@ -15,62 +15,68 @@ import com.mysql.jdbc.Connection;
 import istic.m2ila.taa.tp1.domain.Person;
 
 public class PersonDAO {
-	
+
 	EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
-    EntityManager manager = factory.createEntityManager();
-    
-	//public PersonDAO() {
-	//	super(manager);
-	//}
+	EntityManager manager = factory.createEntityManager();
 
-	//@Override
-	public boolean create(Person obj) {
-		// TODO Auto-generated method stub
-		return false;
+	// public PersonDAO() {
+	// super(manager);
+	// }
+
+	// Person DAO
+	// @Override
+	public boolean create(Person p) {
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		manager.persist(p);
+		tx.commit();
+		return true;
 	}
 
-	//@Override
-	public boolean delete(Person obj) {
-		// TODO Auto-generated method stub
-		return false;
+	// @Override
+	public boolean delete(Person p) {
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		manager.remove(p);
+		tx.commit();
+		return true;
 	}
 
-	//@Override
-	public boolean update(Person obj) {
-		// TODO Auto-generated method stub
-		return false;
+	// @Override
+	public boolean update(Person p) {
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		manager.merge(p);
+		tx.commit();
+		return true;
 	}
-
 
 	public List<Person> findAll() {
 		List<Person> result = new ArrayList<Person>();
 		try {
-			result = manager.createQuery("select p from Person as p")
-					.getResultList();
+			result = manager.createQuery("select p from Person as p").getResultList();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return result;
 	}
-	
+
 	public Optional<Person> findOneById(long id) {
 		Optional<Person> person = Optional.empty();
 		try {
 			person = Optional.of(manager.createQuery("select p from Person as p where p.id = :id", Person.class)
-					.setParameter("id", id)
-					.getSingleResult());
+					.setParameter("id", id).getSingleResult());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return person;
 	}
-	
+
 	public Optional<Person> findOneByName(String name) {
 		Optional<Person> person = Optional.empty();
 		try {
 			person = Optional.of(manager.createQuery("select p from Person as p where p.name = :name", Person.class)
-					.setParameter("name", name)
-					.getSingleResult());
+					.setParameter("name", name).getSingleResult());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/main/java/istic/m2ila/taa/tp1/web/rest/PersonResource.java
+++ src/main/java/istic/m2ila/taa/tp1/web/rest/PersonResource.java
@@ -1,8 +1,13 @@
 package istic.m2ila.taa.tp1.web.rest;
 
 import java.util.List;
+import java.util.Optional;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,18 +32,76 @@ public class PersonResource {
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getById(@PathParam("id") String id) {
-		return personDao.findOneById(Long.parseLong(id))
-				.map(e -> Response.ok().entity(e).build())
+		return personDao.findOneById(Long.parseLong(id)).map(e -> Response.ok().entity(e).build())
 				.orElse(Response.status(Response.Status.NOT_FOUND).build());
 	}
-	
+
 	@GET
 	@Path("name/{name}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getByName(@PathParam("name") String name) {
-		return personDao.findOneByName(name)
-				.map(e -> Response.ok().entity(e).build())
+		return personDao.findOneByName(name).map(e -> Response.ok().entity(e).build())
 				.orElse(Response.status(Response.Status.NOT_FOUND).build());
+	}
+
+	@PUT
+	@Path("{name}/{firstname}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public void create(@PathParam("name") String name, @PathParam("firstname") String firstname) {
+		Person p = new Person();
+		p.setName(name);
+		p.setFirstname(firstname);
+		personDao.create(p);
+	}
+
+	@POST
+	@Path("{id}/{name}/{firstname}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public void update(@PathParam("id") String id, @PathParam("name") String name,
+			@PathParam("firstname") String firstname) {
+		Optional<Person> p = personDao.findOneById(Long.parseLong(id));
+		if (p.isPresent()) {
+			Person pp = p.get();
+			pp.setName(name);
+			pp.setFirstname(firstname);
+			personDao.update(pp);
+		}
+	}
+
+	@POST
+	@Path("{id}/{name}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public void updateName(@PathParam("id") String id, @PathParam("name") String name)  {
+		Optional<Person> p = personDao.findOneById(Long.parseLong(id));
+		if (p.isPresent()) {
+			Person pp = p.get();
+			pp.setName(name);
+			personDao.update(pp);
+		}
+	}
+	
+	@POST
+	@Path("{id}//{firstname}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public void updateFirstname(@PathParam("id") String id, 
+			@PathParam("firstname") String firstname) {
+		Optional<Person> p = personDao.findOneById(Long.parseLong(id));
+		if (p.isPresent()) {
+			Person pp = p.get();
+			pp.setFirstname(firstname);
+			personDao.update(pp);
+		}
+	}
+	
+	@DELETE
+	@Path("{id}")
+	@Consumes({ MediaType.APPLICATION_JSON })
+	public void delete(@PathParam("id") String id) {
+		Optional<Person> p = personDao.findOneById(Long.parseLong(id));
+		if (p.isPresent()) {
+			Person pp = p.get();
+			personDao.delete(pp);
+		}
 	}
 
 }
